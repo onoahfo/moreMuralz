7267bd51e765b96aab78b88d6bcd1f274ebcaa06
Jordan <jtfranklin04203@gmail.com>
2019-07-23 12:52:24 -0400

    adding our routes to everything so now there is a landing page

--- routes/user.js
+++ routes/user.js
@@ -3,16 +3,16 @@ const router  = express.Router();
 const isAuthenticated = require('../config/middleware/isAuthenticated');
 const userController = require('../controllers/user');
 
-router.get( '/user/signup', userController.register );
+router.get( '/register', userController.register );
 
 router.get( '/user/login', userController.userLogin );
-
-router.post( '/user/signup', userController.signup );
-
-router.post( '/user/login', userController.userSignin );
 
 router.get('/profile', isAuthenticated, userController.userProfile );
 
 router.get('/logout', userController.userLogout);
+
+router.post( '/user/signup', userController.signup );
+
+router.post( '/user/login', userController.userSignin );
 
 module.exports = router;
