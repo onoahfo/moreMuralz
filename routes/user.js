fdd69e53814f09d119ca8361110b9d8cb576d3c6
Noah <onoahfo@gmail.com>
2019-07-24 01:11:29 -0400

    creating login and moved img to public

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,11 @@ const router  = express.Router();
 const isAuthenticated = require('../config/middleware/isAuthenticated');
 const userController = require('../controllers/user');
 
-router.get( '/user/signup', userController.register );
+router.get( '/user/register', userController.register );
 
 router.get( '/user/login', userController.userLogin );
 
-router.post( '/user/signup', userController.signup );
+router.post( '/user/register', userController.signup );
 
 router.post( '/user/login', userController.userSignin );
 
