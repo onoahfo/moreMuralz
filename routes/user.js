05ad10729f63cda46b7ce68e3dafe0f9d2bd5c8e
Noah <onoahfo@gmail.com>
2019-07-24 01:51:42 -0400

    fixing routes

--- routes/user.js
+++ routes/user.js
@@ -3,19 +3,19 @@ const router  = express.Router();
 const isAuthenticated = require('../config/middleware/isAuthenticated');
 const userController = require('../controllers/user');
 
+// get 
 router.get( '/user/register', userController.register );
 
 router.get( '/user/login', userController.userLogin );
-// router.get( '/login', (req,res) => {
-//     res.render('login');
-// });
-
-router.post( '/user/register', userController.signup );
-
-router.post( '/user/login', userController.userSignin );
 
 router.get('/profile', isAuthenticated, userController.userProfile );
 
 router.get('/logout', userController.userLogout);
+
+//post
+
+router.post( '/user/register', userController.signup );
+
+router.post( '/user/login', userController.userSignin );
 
 module.exports = router;
