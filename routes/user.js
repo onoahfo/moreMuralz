42a0fcde26faf0b0a0fd7dbf14a985c83cabf6fe
Jordan <jtfranklin04203@gmail.com>
2019-07-25 10:11:14 -0400

    modified these

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const router  = express.Router();
+const bcrypt          = require('bcryptjs')
+const express         = require('express');
+const router          = express.Router();
 const isAuthenticated = require('../config/middleware/isAuthenticated');
-const userController = require('../controllers/user');
+const userController  = require('../controllers/user');
 
 // get 
 router.get( '/register', userController.register );
 
 router.get( '/login', userController.login );
 
+
+// add (isAuthenticated) to userController), so that we are able to block our any other
+// person from trying to get to this page without signing in
+// and redirect them back to the home page
 router.get('/user/profile', userController.userProfile );
 
 router.get('/user/logout', userController.userLogout);
