88577929b9c4f0eb30db2b6e43f601efcecff147
Noah <onoahfo@gmail.com>
2019-07-30 01:55:30 -0400

    adding spot data and demo page

---
+++ spotdata.js
@@ -0,0 +1,11 @@
+const db = require('./models');
+
+
+db.Spot.create({
+    id: 1,
+    title: "Lot11",
+    address: "NW 1st St & Northwest 3rd Avenue, Miami, FL 33128",
+    image: "https://lh5.googleusercontent.com/p/AF1QipMVB--Gi2Q9s9yO7LvDPsri8ZsjAJaVUcQrhjf5=w408-h306-k-no",
+    besttime: "6 pm",
+    description: "Pared-down urban skate park beneath the highway, offering ramps & picnic tables.",
+})
